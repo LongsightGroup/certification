f6611b857831d4f25f306aff34b627126b00c80f
bbailla2@uwo.ca <bbailla2@uwo.ca@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2013-02-28 22:28:57 +0000

    OWL-440 - hiding expiration date from page 3 when appropriate
    git-svn-id: https://source.sakaiproject.org/svn/msub/uwo.ca/certification/trunk@120553 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- tool/src/main/java/com/rsmart/certification/tool/utils/CertificateToolState.java
+++ tool/src/main/java/com/rsmart/certification/tool/utils/CertificateToolState.java
@@ -32,6 +32,7 @@ import com.rsmart.certification.api.DocumentTemplateService;
 
 import com.rsmart.certification.api.criteria.CriteriaTemplate;
 import com.rsmart.certification.api.criteria.Criterion;
+import com.rsmart.certification.impl.hibernate.criteria.gradebook.WillExpireCriterionHibernateImpl;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -256,6 +257,24 @@ public class CertificateToolState
 
 	public void setPredifinedFields(Map<String, String> predifinedFields) 
 	{
+		boolean criteriaContainWechi = false;
+		
+		CertificateDefinition certDef = getCertificateDefinition();
+		Set<Criterion> awardCriteria = certDef.getAwardCriteria();
+		for (Criterion criterion : awardCriteria)
+		{
+			if (criterion instanceof WillExpireCriterionHibernateImpl)
+			{
+				criteriaContainWechi = true;
+			}
+		}
+		
+		
+		//hate to hard code this. I'd grab it from GradebookVariableResolver, but we can't access impl
+		String expireDate = "${cert.expiredate}";
+		boolean fieldValuesContainWechi = certDef.getFieldValues().values().contains(expireDate);
+		
+		
 		Map<String, String> temp = null;
 		if(predifinedFields != null)
 		{
@@ -267,7 +286,17 @@ public class CertificateToolState
 				{
 					newKey = "${"+key+"}";
 				}
-				temp.put(newKey, predifinedFields.get(key));
+				//if the award criteria don't contain a wechi and the key is the expiry date, skip it
+				
+				/* Anything other than the expiry date gets added.
+				 * If the key is the expiry date, we only add it if the award criteria contain wechi
+				 * or if the field values contain wechi (for whatever reason).
+				 * So we add it if 
+				 * key != expiry date OR criteriacontainswechi OR fieldvaluescontainwechi*/ 
+				if (!expireDate.equals(newKey) || criteriaContainWechi || fieldValuesContainWechi)
+				{
+					temp.put(newKey, predifinedFields.get(key));
+				}
 			}
 		}
 		this.predifinedFields = temp;
